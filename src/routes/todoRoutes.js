582f304f6b0b7c56760995a4dd0bb1523973e961
Obiri254 <anthonymburu4449@gmail.com>
2025-05-28 21:58:16 +0300

    finalise the code

--- src/routes/todoRoutes.js
+++ src/routes/todoRoutes.js
@@ -14,17 +14,30 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
     const { task } = req.body
     const insertTodo = db.prepare(`INSERT INTO todos(user_id, task) VALUES(?, ?)`)
-    insertTodo.run(req.userId, task)
+    const result = insertTodo.run(req.userId, task)
 
-    res.json({id: insertTodo.lastId, task, completed: 0})
+    res.json({id: result.lastInsertRowid, task, completed: 0})
 })
 // Update a todo
 router.put('/:id', (req, res) => {
+    const { completed } = req.body
+    const { id } = req.params
 
+    const updatedTodo = db.prepare(`UPDATE todos SET completed = ? WHERE id = ?`)
+
+    updatedTodo.run(completed, id)
+
+    res.json({message: "Todo completed"})
 })
 // Delete a todo
 router.delete('/:id', (req, res) => {
+    const { id } = req.params
+    const userId = req.userId
+    const deleteTodo = db.prepare(`DELETE FROM todos WHERE id = ? AND user_id = ?`)
 
+    deleteTodo.run(id, userId)
+
+    res.send({message: "Todo deleted"})
 })
  
 export default router
